24eb5f38960e058742802f6e276cedfe5fd90caa
girishramnani <girishramnani95@gmail.com>
2015-05-04 17:10:58 +0530

    reformated the code and added stub

--- scrapper/Snippet.py
+++ scrapper/Snippet.py
@@ -4,17 +4,15 @@ import re
 
 
 class Snippet(object):
-
-    def __init__(self,filter=None,**kwargs):
-
+    def __init__(self, filter=None, **kwargs):
         if filter is None:
-            statement =[r"(?P<{}>{})".format(key,val) for key,val in kwargs.items()]
+            statement = [r"(?P<{}>{})".format(key, val) for key, val in kwargs.items()]
             self.statement = r"".join(statement)
             self.filter = re.compile(self.statement)
-            self.spoj_filter=False
+            self.spoj_filter = False
         else:
             self.filter = filter
-            self.spoj_filter=True
+            self.spoj_filter = True
 
     def __str__(self):
         return self.statement if not self.spoj_filter else "SPOJ filter"
@@ -23,15 +21,15 @@ class Snippet(object):
         self.__str__()
 
     @classmethod
-    def SPOJ(cls,data):
-        result= re.finditer(r"""
+    def SPOJ(cls, data):
+        result = re.finditer(r"""
         (?P<id><td[\s]* class\=\"text\-center\">\s*\d+\s*</td>)
         ([\s\w\=\<\"]*\>)
         (<a\s* href\=\"[\w/\.\:\"\s]*>)
         (?P<name>[\w\s\,]*)
-        """,data,re.X|re.M)
+        """, data, re.X | re.M)
         return result
 
-    def get_data_dicts(self,data):
-        data= re.sub("[\\n\\t]+","",data)
+    def get_data_dicts(self, data):
+        data = re.sub("[\\n\\t]+", "", data)
         yield from self.filter(data)
---
+++ tests/Data_fetcher_test.py
@@ -0,0 +1,12 @@
+__author__ = 'Girish'
+
+import unittest
+
+
+class Data_fetcher_test(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
