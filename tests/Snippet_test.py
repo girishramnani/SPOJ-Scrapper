6cce21e046073b1edf6fbd8f86eb588ed13bd382
girish ramnani <girishramnani95@gmail.com>
2015-10-15 20:48:05 +0530

    Update Snippet_test.py

--- tests/Snippet_test.py
+++ tests/Snippet_test.py
@@ -2,10 +2,15 @@ __author__ = 'Girish'
 
 import unittest
 import requests
-import os
-import sys
 
-from ..scrapper.Snippet import Snippet
+import sys
+import os
+
+PACKAGE_PARENT = '..'
+SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
+
+from scrapper.Snippet import Snippet
 
 
 
