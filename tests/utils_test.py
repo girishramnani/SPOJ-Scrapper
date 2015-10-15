e879be9ace4102371ff852ac4cf9775a750b9769
girish ramnani <girishramnani95@gmail.com>
2015-10-15 20:53:57 +0530

    Update utils_test.py

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -4,6 +4,12 @@ __author__ = 'Girish'
 #contains the test for the utils file , basically the chache function
 
 import unittest
+import sys
+import os
+
+PACKAGE_PARENT = '..'
+SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 
 from scrapper import Utils
 class MyTestCase(unittest.TestCase):
